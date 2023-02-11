200a4690507492923560f138159d07705cf050ca
Roman Koshchei <romankoshchei@gmail.com>
2023-02-11 18:40:09 +0200

    init

---
+++ ShowDocker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShowDocker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
